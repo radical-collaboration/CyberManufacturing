0acea626490f5fbedbf8f710c82ea347cae1dd43
ChaiSam <sampatchaitanya@gmail.com>
2017-08-02 18:04:20 -0400

    Removed old hybrid folder

--- src/PBM_hybrid_MPI+OMP_code/copy2amarel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-scp -r *.cpp *.h yb177@amarel1.hpc.rutgers.edu:/home/yb177/parallelCpp/
-
-#scp -r *.h yb177@amarel1.hpc.rutgers.edu:/home/yb177/parallelCpp/
-
-echo "copying done"
--- src/PBM_hybrid_MPI+OMP_code/timeStamp.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIMESTAMP_H
-#define TIMESTAMP_H
-
-double cpu_time ( );
-int *time_numbers ( );
-void timestamp ( );
-char *timestring ( );
-
-#endif //TIMESTAMP_H

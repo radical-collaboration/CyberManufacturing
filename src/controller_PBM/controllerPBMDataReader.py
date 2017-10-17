4700588ffef23912a3c991eb0bb5b55f72009b9c
ChaiSam <sampatchaitanya@gmail.com>
2017-10-17 17:52:46 -0400

    controller PBM data reader added

--- src/controller_PBM/controllerPBMDataReader.py
+++ src/controller_PBM/controllerPBMDataReader.py
@@ -5,4 +5,27 @@ Created on Tue Oct  17 16:19:29 2017
 
 @author: Chaitanya Sampat
 """
+'''
+This file accesses the folder that contains the output files of the PBM and waits for the data
+file to be dumped from the PBM execution and reads the d50 and # of particle data and provides the
+information to the interpretor class for further processing
+'''
+import numpy as np
+import os
+import glob
 
+# class to locate the PBM dump file, identify the timestamp and store the values of the same
+
+class controllerPBMDataReader(object):
+
+    #Constructor
+    def __init__(self, prev_timestamp, compartments, bins1, bins2):
+        self.p_ts = prev_timestep # saves the last timestamp from the PBM
+        self.compartments= compartments # number of comparments inside the PBM
+        self.bins1 = bins1 # number of segregations of bins of solid 1 in PBM
+        self.bins2 = bins2 # number of segregations of bins of solid 2 in PBM
+
+
+    # function to look for the files in the PBM output folder
+    def file_finder(self, last_ts):
+        output_PBM_path = os.getcwd() + '../twoway_PBM/csvDump'

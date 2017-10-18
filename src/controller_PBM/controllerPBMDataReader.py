4af382bf7de87f6d44c407a1a87f5c45faaf4327
ChaiSam <sampatchaitanya@gmail.com>
2017-10-18 16:02:58 -0400

    added file locator

--- src/controller_PBM/controllerPBMDataReader.py
+++ src/controller_PBM/controllerPBMDataReader.py
@@ -22,7 +22,7 @@ class controllerPBMDataReader(object):
 
     #Constructor
     def __init__(self, prev_timestamp, compartments, bins1, bins2):
-        self.p_ts = prev_timestep # saves the last timestamp from the PBM
+        self.p_ts = prev_timestamp # saves the last timestamp from the PBM
         self.compartments= compartments # number of comparments inside the PBM
         self.bins1 = bins1 # number of segregations of bins of solid 1 in PBM
         self.bins2 = bins2 # number of segregations of bins of solid 2 in PBM
@@ -33,22 +33,37 @@ class controllerPBMDataReader(object):
         output_PBM_path = os.getcwd()
         output_PBM_path = os.chdir('../twoway_PBM/csvDump/')
         output_PBM_path = os.getcwd()
-        file_list = glob.glob("*.csv")
-        for file1 in file_list:
+        file_list_particles = glob.glob("particles_*.csv")
+        file_list_particles.sort()
+        file_list_d50 = glob.glob("d50_*.csv")
+        file_list_d50.sort()
+        new_ts = last_ts
+        for file1 in file_list_d50:
             try:
-                d50_find_nextfile = re.search('d50_(.+?).csv', file1).group(1)
+                d50_nextfile_timestamp = re.search('d50_(.+?).csv', file1).group(1)
+                particles_nextfile = 'particles_' + d50_nextfile_timestamp + '.csv'
+                d50_nextfile = 'd50_' + d50_nextfile_timestamp + '.csv'
+                if (os.path.isfile(particles_nextfile) and os.path.isfile(d50_nextfile) and np.float(d50_nextfile_timestamp) > last_ts):
+                    new_ts = np.float(d50_nextfile_timestamp)
+                    break
+                else:
+                    continue
             except AttributeError:
                 d50_nextfile = ''
+                particles_nextfile = ''
                 #time.sleep(0.2)
+                '''
             try:
                 particles_nextfile = re.search('particles_(+.?).csv', file1).group(1)
             except:
                 #time.sleep(0.2)
                 particles_nextfile = ''
-            if (np.float(d50_nextfile) > last_ts and np.float(particle_nextfile) > last_ts):
-                break
-            elif (file1 != file_list[-1]):
+                '''
+            if (file1 != file_list_d50[-1]):
                 continue
             else:
                 time.sleep(0.1)
+        return new_ts
 
+a = controllerPBMDataReader(10,4,16,16)
+t = a.file_finder(30)

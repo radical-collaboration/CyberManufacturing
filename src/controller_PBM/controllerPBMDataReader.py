2fd990bff7eb1c32e5b108000e7ca8b495a201bf
ChaiSam <sampatchaitanya@gmail.com>
2017-10-18 15:14:07 -0400

    added file locator

--- src/controller_PBM/controllerPBMDataReader.py
+++ src/controller_PBM/controllerPBMDataReader.py
@@ -13,6 +13,8 @@ information to the interpretor class for further processing
 import numpy as np
 import os
 import glob
+import re
+import time
 
 # class to locate the PBM dump file, identify the timestamp and store the values of the same
 
@@ -26,6 +28,27 @@ class controllerPBMDataReader(object):
         self.bins2 = bins2 # number of segregations of bins of solid 2 in PBM
 
 
-    # function to look for the files in the PBM output folder
+    # function to look for the next file in the PBM output folder after the PBM time step
     def file_finder(self, last_ts):
-        output_PBM_path = os.getcwd() + '../twoway_PBM/csvDump'
+        output_PBM_path = os.getcwd()
+        output_PBM_path = os.chdir('../twoway_PBM/csvDump/')
+        output_PBM_path = os.getcwd()
+        file_list = glob.glob("*.csv")
+        for file1 in file_list:
+            try:
+                d50_find_nextfile = re.search('d50_(.+?).csv', file1).group(1)
+            except AttributeError:
+                d50_nextfile = ''
+                #time.sleep(0.2)
+            try:
+                particles_nextfile = re.search('particles_(+.?).csv', file1).group(1)
+            except:
+                #time.sleep(0.2)
+                particles_nextfile = ''
+            if (np.float(d50_nextfile) > last_ts and np.float(particle_nextfile) > last_ts):
+                break
+            elif (file1 != file_list[-1]):
+                continue
+            else:
+                time.sleep(0.1)
+

1e8a0a1be5625b277fae5ca8e981b0bb9f056774
ChaiSam <sampatchaitanya@gmail.com>
2017-10-20 16:15:24 -0400

    added file locator

--- src/controller_PBM/controllerPBMDataReader.py
+++ src/controller_PBM/controllerPBMDataReader.py
@@ -15,6 +15,7 @@ import os
 import glob
 import re
 import time
+import pandas as pd
 
 # class to locate the PBM dump file, identify the timestamp and store the values of the same
 
@@ -26,10 +27,12 @@ class controllerPBMDataReader(object):
         self.compartments= compartments # number of comparments inside the PBM
         self.bins1 = bins1 # number of segregations of bins of solid 1 in PBM
         self.bins2 = bins2 # number of segregations of bins of solid 2 in PBM
+        self.d50_overtime = np.array((1,compartments))
+        self.last_timeindex = 2
 
 
     # function to look for the next file in the PBM output folder after the PBM time step
-    def file_finder(self, last_ts):
+    def nextfile_time_finder(self, last_ts):
         output_PBM_path = os.getcwd()
         output_PBM_path = os.chdir('../twoway_PBM/csvDump/')
         output_PBM_path = os.getcwd()
@@ -37,7 +40,7 @@ class controllerPBMDataReader(object):
         file_list_particles.sort()
         file_list_d50 = glob.glob("d50_*.csv")
         file_list_d50.sort()
-        new_ts = last_ts
+        new_ts = 0.0
         for file1 in file_list_d50:
             try:
                 d50_nextfile_timestamp = re.search('d50_(.+?).csv', file1).group(1)
@@ -65,5 +68,33 @@ class controllerPBMDataReader(object):
                 time.sleep(0.1)
         return new_ts
 
+    # method to extract data from the d50 csv file and save the data uptill a given time step
+    def data_d50_extractor(self, last_ts):
+        new_ts = self.nextfile_time_finder(last_ts)
+        filetoread_d50 = 'd50_' + str(new_ts) + '.csv'
+        temp_d50 = np.zeros(1,self.compartments)
+        with open(filetoread_d50, 'rb') as d50_current_file:
+            read_d50 = pd.read_csv(d50_current_file, header = 0)
+            ts_len = len(read_d50.Time)
+            if self.timeindex + 1
+            for x in range(1, ts_len):
+                for i in range(0,self.compartments):
+                    if np.isnan(read_d50.iloc[x][i]):
+                        temp_d50[i] = 0
+                    else:
+                        temp_d50[i] = read_d50.iloc[x][i]
+                    self.d50_overtime.append(temp_d50)
+
+
+
+
+
+
+
+
+
+
+
 a = controllerPBMDataReader(10,4,16,16)
-t = a.file_finder(30)
+t = a.nextfile_time_finder(10.08)
+
